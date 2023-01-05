5f6d6af3e14888db8ec6cb7d06e3e9744b91b9e1
Lukáš Lalinský <lalinsky@gmail.com>
2023-01-05 07:53:22 +0100

    Extract pyacoustid version from setup.py

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-PY_ACOUSTID_VERSION=1.2.2
-
-mkdir -p chromaprint
+PY_ACOUSTID_VERSION=$(grep version setup.py  | perl -pe "s{\s+version='(.*)',\s+}{\\1}")
 
 curl -sSL -o chromaprint/__init__.py https://raw.githubusercontent.com/beetbox/pyacoustid/v$PY_ACOUSTID_VERSION/chromaprint.py
